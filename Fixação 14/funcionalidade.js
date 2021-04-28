e88e4e7792d1a0224d8cb725679fd441334411ca
Ana Cláudia Silva <70230171+AnaSales01@users.noreply.github.com>
2021-04-28 18:49:38 -0300

    Adicionando mais atividades feitas para a ementa

---
+++ Fixação 14/funcionalidade.js
@@ -0,0 +1,42 @@
+let creme = [
+    {
+        nome: "Aloe e Vera",
+        tipo: "creme para pele",
+        marca: "Dior",
+        valor: "R$ 25,00",
+        quantidade: "0",
+    },
+    {
+        nome: "Rosas",
+        tipo: "creme para os pés",
+        marca: "Chanel",
+        valor: "R$ 50,00",
+        quantidade: "0",
+    },
+    {
+        nome: "Girassol",
+        tipo: "creme para rosto",
+        marca: "Avon",
+        valor: "R$ 35,00",
+        quantidade: "0",
+    },
+    {
+        nome: "Malaleuca",
+        tipo: "creme para mãos",
+        marca: "Nivia",
+        valor: "R$ 15,00",
+        quantidade: "0",
+    },
+    {
+        nome: "Uva",
+        tipo: "creme para pele",
+        marca: "Loreal",
+        valor: "R$ 45,00",
+        quantidade: "0",
+    }
+];
+
+let exibir = document.getElementById("mostrar")
+
+
+
